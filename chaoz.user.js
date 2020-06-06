2a71704ab3335dcf6c5f41f80f5d3620b6710d82
Idler1972 <odd@veibust.no>
2020-06-06 08:40:39 +0200

    Update chaoz.user.js
    Removed autoscan part, will add to separate script

--- chaoz.user.js
+++ chaoz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chaoz Script
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.2
 // @description  Chaoz TM Script
 // @author       You
 // @match        https://*.planetarion.com/*
@@ -30,13 +30,7 @@
         var page = '';
         var view = '';
         var tick = 0;
-        var minReloadTime = 10; //Minimum time for auto reload, in seconds
-        var maxReloadTime = 120; //Minimum time for auto reload, in seconds
-        var runAutoScan = true; //Set false to stop auto scan on alliance_scans page
-        var autoReloadScanPage = true; //Set false to stop auto reload on alliance_scans page
-        var alrtScope;
         var globalDependencies = ['jQuery', 'get_cookie', 'get_ships_cookie', 'PA'];
-
 
         if (doc.readyState == 'complete') {
             checkDeps(true);
@@ -82,7 +76,6 @@
                 var wvs = ($j('a[target="scan"]').map((id, scn) => getScanId(scn.search))).toArray();
                 postScanLinks([... new Set(unique(wvs))]);
             } else if(page == 'alliance_scans') {
-                autoScan();
                 var scns = ($j('a[target="wavescan"]').map((id, scn) => getScanId(scn.search))).toArray();
                 postScanLinks([... new Set(unique(scns))]);
             }
@@ -126,45 +119,6 @@
             }
         }
         
-        function autoScan() {
-            try {
-                var urlRandomizer = Math.floor((Math.random() * 9999999999) + 1000000000);
-                if(runAutoScan) {
-                    //Override alert box, to avoid "hang" of auto reload
-                    if (typeof unsafeWindow === "undefined") {
-                        alrtScope = window;
-                    } else {
-                        alrtScope = unsafeWindow;
-                    }
-                    alrtScope.alert = function (str) {
-                        console.log ("Intercepted alert: ", str);
-                    };
-                    //Click each scan link
-                    var hasScanned = false;
-                    $j.each($j('a[class^="link_"]'), function(ind, obj) {
-                        $j(this).click();
-                        hasScanned = true;
-                    });
-                    //Force reload in 10 sec to see if there are added more scans
-                    if(hasScanned) {
-                        setTimeout(function(){
-                            window.location.href = 'alliance_scans.pl?rn=' + urlRandomizer + '#tab2';
-                        }, 10000);
-                    }
-                }
-                if(autoReloadScanPage) {
-                    var randomTimeToReload = Math.floor(Math.random() * (Math.floor(maxReloadTime*1000) - Math.ceil(minReloadTime*1000) + 1)) + Math.ceil(minReloadTime*1000);
-                    setTimeout(function(){
-                        window.location.href = 'alliance_scans.pl?rn=' + urlRandomizer + '#tab2';
-                    }, randomTimeToReload);
-                    console.log('Auto reload in ' + Math.floor(randomTimeToReload/1000) + ' seconds.');
-                }
-            } 
-            catch(autoError) {
-                console.log('An error occured: ', autoError.message);
-            }
-        }
-        
         function postScanLinks(scan_ids) {
             if(scan_ids.length > 0) {
                 var xhr = new XMLHttpRequest();
@@ -174,8 +128,6 @@
                 xhr.send(JSON.stringify({scan_ids:scan_ids}));
             }
         }
-
-
 
         function menu_updateMenu() {
             //Add Remaining Ticks
