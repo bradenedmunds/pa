18c10e5f73579dd69ec30a07f7e5964dd2cb5339
bradenedmunds <braden.edmunds@gmail.com>
2020-08-17 16:58:35 -0600

    Update chaoz.user.js

--- chaoz.user.js
+++ chaoz.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chaoz Script
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Chaoz TM Script
 // @author       You
 // @match        https://*.planetarion.com/*
